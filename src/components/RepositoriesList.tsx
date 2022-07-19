8dd4ea24a7d946880e0f60d038c65d99292e9f32
RJD02 <dulangeraviraj@gmail.com>
2022-07-20 03:19:10 +0530

    Add components

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepo } = useActions();
+  const { data, error, loading } = useTypedSelector((s) => s.repo);
+  console.log(state);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    searchRepo(term);
+  };
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && (
+        <ul>
+          {data.map((ele) => (
+            <li key={ele}>{ele}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
